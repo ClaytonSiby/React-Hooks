156105da23a842b97b6d89dd5ec8fd008d358fe4
ClaytonSiby <claytonsiby@gmail.com>
2021-01-09 19:31:10 +0200

    Demonstrate how to create & consume a context value

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import Counter from './components/counter';
-import PrevState from './components/PrevState';
-import ObjectAsState from './components/ObjectAsState';
-import ArrayAsState from './components/ArrayAsState';
-import UseEffect from './components/UseEffect';
-import ClassMouse from './components/ClassMouse';
-import MouseContainer from './components/MouseContainer';
-import IntervalClassCounter from './classComponents/IntervalClassCounter';
-import IntervalHookCounter from './components/IntervalHookCounter';
-import FullName from './components/FullName';
-import DataFetching from './components/DataFetching';
-import './App.css';
+import React from 'react'
+import Counter from './components/counter'
+import PrevState from './components/PrevState'
+import ObjectAsState from './components/ObjectAsState'
+import ArrayAsState from './components/ArrayAsState'
+import UseEffect from './components/UseEffect'
+import ClassMouse from './components/ClassMouse'
+import MouseContainer from './components/MouseContainer'
+import IntervalClassCounter from './classComponents/IntervalClassCounter'
+import IntervalHookCounter from './components/IntervalHookCounter'
+import FullName from './components/FullName'
+import DataFetching from './components/DataFetching'
+import ComponentC from './classComponents/ComponentC'
+import './App.css'
 
-function App() {
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
+
+function App () {
   return (
-    <div className="App">
+    <div className='App'>
       {/* <Counter />
       <PrevState />
       <ObjectAsState />
@@ -24,9 +29,14 @@ function App() {
       {/* <IntervalClassCounter />
       <IntervalHookCounter /> */}
       {/* <FullName /> */}
-      <DataFetching />
+      {/* <DataFetching /> */}
+      <UserContext.Provider value={'Clayton'}>
+        <ChannelContext.Provider value={'Software Development'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
